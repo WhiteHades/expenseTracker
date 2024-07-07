6e143caa5f31b4559811f87ec5ba16129ca9c6b0
Efaz Mohammed <44260523+WhiteHades@users.noreply.github.com>
2024-07-07 18:31:57 +0200

    added components

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,16 @@
-<template>My App</template>
+<template>
+    <Header/>
+    <div class="container">
+
+    </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+
+export default {
+  components: {
+    Header
+  }
+}
+</script>
